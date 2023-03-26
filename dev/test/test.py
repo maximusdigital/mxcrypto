0f259119e6d473cb48d306f7f96d2e9827c29696
max <mm@maximusinvestment.de>
2023-03-26 15:42:33 +0200

    init

--- dev/test/test.py
+++ dev/test/test.py
@@ -1,14 +1,17 @@
 from web3 import Web3
 
-binance_testnet_rpc_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+binance_testnet_rpc = "https://data-seed-prebsc-1-s1.binance.org:8545/"
 
-web3 = Web3(Web3.HTTPProvider(binance_testnet_rpc_url))
-print(f"Is connected: {web3.is_connected()}")  # Is connected: True
+web3_binance = Web3(Web3.HTTPProvider(binance_testnet_rpc))
+print(f"Is connected: {web3_binance.is_connected()}")  # Is connected: True
 # С подключением ва
 
+print(f"gas price: {web3_binance.eth.gas_price} BNB")  # кол-во Wei за единицу газа
+print(f"current block number: {web3_binance.eth.block_number}")
+print(f"number of current chain is {web3_binance.eth.chain_id}")  # 97
 
-arb_url = "https://arb1.arbitrum.io/rpc"
-web3_arb = Web3(Web3.HTTPProvider(arb_url))
-web3_arb.is_connected()
 
-print(f"Is connected: {web3_arb.is_connected()}")  # Is connected: True
+arbitrum_rpc = "https://arb1.arbitrum.io/rpc"
+web3_arbitrum = Web3(Web3.HTTPProvider(arbitrum_rpc))
+print(f"Is connected: {web3_arbitrum.is_connected()}")  # Is connected: True
+
